63c86f6ae1cd7d938ca7b9c4c8ec84d0cff91de7
flunoah <dragonsuny0305@gmail.com>
2026-06-03 01:23:04 +0900

    서버 주소 Render로 변경

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "https://olivemate-api.onrender.com/api/:path*'
+        destination: "https://olivemate-api.onrender.com/api/:path*",
       },
     ];
   },
 };
 
-module.exports = nextConfig
+export default nextConfig;
